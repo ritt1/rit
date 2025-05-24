a102b8d7dad1d77d23c7334bcc827fc3d9185ddc
rit <164156809+ritt1@users.noreply.github.com>
2025-05-25 01:06:18 +0530

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt
+
+X, y = load_breast_cancer(return_X_y=True)
+model = DecisionTreeClassifier().fit(X, y)
+
+pred = model.predict([X[0]])
+print("Class:", "Benign" if pred[0] == 1 else "Malignant")
+
+plot_tree(model, filled=True)
+plt.title("Decision tree-breast cancer dataset")
+plt.show()
