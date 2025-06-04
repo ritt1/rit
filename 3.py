e0a1a22e3c08b233704b35b9a3739252f556ca36
rit <164156809+ritt1@users.noreply.github.com>
2025-06-04 11:17:27 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,6 +2,5 @@ from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 X, y = load_iris(return_X_y=True)
-X_pca = PCA(2).fit_transform(X)
-for i, c in enumerate('rgb'): plt.scatter(*X_pca[y==i].T, c=c, label=load_iris().target_names[i])
+X_pca = PCA(2).fit_transform(X);for i, c in enumerate('rgb'): plt.scatter(*X_pca[y==i].T, c=c, label=load_iris().target_names[i])
 plt.legend(); plt.title('PCA - Iris'); plt.show()
