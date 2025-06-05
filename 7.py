5dde7c3dc766fc775bb6361e6dae9dc59b5d9e69
rit <164156809+ritt1@users.noreply.github.com>
2025-06-05 14:42:49 +0530

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_boston
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+X, y = load_boston(return_X_y=True); X = X[:, [5]]
+
+plt.scatter(X, y, s=10,label='actual');
+plt.plot(X, m.predict(X), 'r',label='predited');
+plt.title("Linear Regression")
+plt.xlabel("Average Rooms per Dwelling");plt.ylabel("House Price")
+plt.legend(); plt.show()
+
+
