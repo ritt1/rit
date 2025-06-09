eb42114145bb49268fcfc6bb09c672b31011bd00
rit <164156809+ritt1@users.noreply.github.com>
2025-06-09 22:47:51 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,9 @@
-import seaborn as sns; from sklearn.datasets import fetch_california_housing as f
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_california_housing as f
 df = f(as_frame=True).frame
-df.hist(figsize=(8,5)); plt.tight_layout(); plt.show()
-df.plot.box(subplots=True, layout=(3,3),figsize=(8,5)); plt.tight_layout(); plt.show()
+df.hist(figsize=(19,11)); plt.tight_layout(); plt.show()
+df.plot.box(subplots=True, layout=(3,3),figsize=(19,11)); plt.tight_layout(); plt.show()
 print(f"{c} outliers:", ((df[c]<q1-1.5*(q3-q1))|(df[c]>q3+1.5*(q3-q1))).sum())
 
 
