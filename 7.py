952aed5e7f614035e3112e68aab2f87cdad932ef
rit <164156809+ritt1@users.noreply.github.com>
2025-06-09 22:55:08 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -3,9 +3,9 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 X, y = load_boston(return_X_y=True); X = X[:, [5]]
-
-plt.scatter(X, y, s=10,label='actual');
-plt.plot(X, m.predict(X), 'r',label='predited');
+m=LinearRegression().fit(X,y)
+plt.scatter(X, y, s=10,label='Actual');
+plt.plot(X, m.predict(X), 'r',label='Predict');
 plt.title("Linear Regression")
 plt.xlabel("Average Rooms per Dwelling");plt.ylabel("House Price")
 plt.legend(); plt.show()
