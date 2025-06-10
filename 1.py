86ea634c32d3a9778687594e9846914269d89442
rit <164156809+ritt1@users.noreply.github.com>
2025-06-10 07:27:51 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,6 +4,8 @@ from sklearn.datasets import fetch_california_housing as f
 df = f(as_frame=True).frame
 df.hist(figsize=(19,11)); plt.tight_layout(); plt.show()
 df.plot.box(subplots=True, layout=(3,3),figsize=(19,11)); plt.tight_layout(); plt.show()
+for c in df: q1,q3=df[c].quantile([.25,.75]); 
 print(f"{c} outliers:", ((df[c]<q1-1.5*(q3-q1))|(df[c]>q3+1.5*(q3-q1))).sum())
+
 
 
