adc36893e3fa6182d36d694d50689b8de5bf8b19
rit <164156809+ritt1@users.noreply.github.com>
2026-03-04 22:13:14 +0530

    Delete 4.py

--- 4.py
+++
@@ -1,8 +0,0 @@
-import csv
-def find_s(f):
-    d = list(csv.reader(open(f)))[1:]
-    h = d[0][:-1]
-    for r in d[1:]:
-        if r[-1].lower()=='yes': h = [x if x==y else '?' for x,y in zip(h,r[:-1])]
-    return h
-print(find_s('training_data.csv'))
